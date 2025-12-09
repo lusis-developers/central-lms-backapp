17f055a7e600f48391bf2158885276d51470ff9f
Diego Reyes <diegorele13@gmail.com>
2025-12-09 01:46:36 -0500

    feat(models): add CommentModel to exported models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,9 @@
 import { UserModel } from "./user.model";
 import { TransactionModel } from "./transaction.model";
+import { CommentModel } from "./comment.model";
 
 export const models = {
   users: UserModel,
   transactions: TransactionModel,
+  comments: CommentModel,
 };
