f81dc837a90152bd8621011c7ce30706e05757a0
Diego Reyes <diegorele13@gmail.com>
2025-12-08 22:20:37 -0500

    feat(routes): add user router to api routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
+import userRouter from "./user.router";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api", router);
+  router.use("/users", userRouter);
 }
 
 export default routerApi;
