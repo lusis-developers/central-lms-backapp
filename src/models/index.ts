645d6c50851141ae5eb34daa124e5a64611f068c
Diego Reyes <diegorele13@gmail.com>
2025-12-08 23:03:17 -0500

    feat(models): add TransactionModel to exported models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,7 @@
 import { UserModel } from "./user.model";
+import { TransactionModel } from "./transaction.model";
 
 export const models = {
   users: UserModel,
+  transactions: TransactionModel,
 };
