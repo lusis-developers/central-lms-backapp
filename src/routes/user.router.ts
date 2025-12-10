f6797ab5655a7482b9fa9f8f52e0f04a71a2102f
Diego Reyes <diegorele13@gmail.com>
2025-12-09 20:00:47 -0500

    feat(user): add getUserById endpoint to user router

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, loginUser, registerFromPayment } from "../controllers/user.controller";
+import { createUser, loginUser, registerFromPayment, getUserById } from "../controllers/user.controller";
 
 const userRouter = Router();
 
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/register-from-payment", registerFromPayment);
+userRouter.get("/:userId", getUserById);
 
 export default userRouter;
