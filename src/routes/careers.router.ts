466b7bcfce6270a98c816f183983cba282aa5e0f
Diego Reyes <diegorele13@gmail.com>
2025-12-11 13:42:02 -0500

    feat(routes): add careers router with CRUD operations and user management

---
+++ src/routes/careers.router.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { createCareer, getCareers, getCareerById, addCourseToCareer, removeCourseFromCareer, assignCareerToUser, enrollUserToCareerCourses, getUserCareers } from "../controllers/careers.controller";
+
+const careersRouter = Router();
+
+careersRouter.post("/", createCareer);
+careersRouter.get("/", getCareers);
+careersRouter.get("/:careerId", getCareerById);
+careersRouter.post("/:careerId/courses/:courseId", addCourseToCareer);
+careersRouter.delete("/:careerId/courses/:courseId", removeCourseFromCareer);
+
+export default careersRouter;
+careersRouter.post("/:careerId/assign/:userId", assignCareerToUser);
+careersRouter.post("/:careerId/enroll/:userId", enrollUserToCareerCourses);
+careersRouter.get("/user/:userId", getUserCareers);
