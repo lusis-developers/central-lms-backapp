6b095e6e0dcecb02e062513993163b86cf73105a
Diego Reyes <diegorele13@gmail.com>
2025-12-11 13:41:53 -0500

    feat(models): add CareerModel to exported models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,9 +1,11 @@
 import { UserModel } from "./user.model";
 import { TransactionModel } from "./transaction.model";
 import { CommentModel } from "./comment.model";
+import { CareerModel } from "./career.model";
 
 export const models = {
   users: UserModel,
   transactions: TransactionModel,
   comments: CommentModel,
+  careers: CareerModel,
 };
