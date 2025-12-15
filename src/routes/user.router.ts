86b6181e2d759a13e1dbbb0106605b428ed9fd30
Diego Reyes <diegorele13@gmail.com>
2025-12-15 12:17:29 -0500

    feat(user): add password change endpoint to user router

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, loginUser, registerFromPayment, getUserById, checkUserByEmail, updateUser } from "../controllers/user.controller";
+import { createUser, loginUser, registerFromPayment, getUserById, checkUserByEmail, updateUser, changePassword } from "../controllers/user.controller";
 
 const userRouter = Router();
 
@@ -9,5 +9,6 @@ userRouter.post("/register-from-payment", registerFromPayment);
 userRouter.get("/exists", checkUserByEmail);
 userRouter.get("/:userId", getUserById);
 userRouter.patch("/:userId", updateUser);
+userRouter.patch("/:userId/password", changePassword);
 
 export default userRouter;
