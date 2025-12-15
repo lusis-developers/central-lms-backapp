0235150e5b723b229f4ae71c4fd96f9bcf012dd3
Diego Reyes <diegorele13@gmail.com>
2025-12-15 15:46:30 -0500

    feat(models): add quiz and quiz submission models to exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,14 @@ import { UserModel } from "./user.model";
 import { TransactionModel } from "./transaction.model";
 import { CommentModel } from "./comment.model";
 import { CareerModel } from "./career.model";
+import { QuizModel } from "./quiz.model";
+import { QuizSubmissionModel } from "./quizSubmission.model";
 
 export const models = {
   users: UserModel,
   transactions: TransactionModel,
   comments: CommentModel,
   careers: CareerModel,
+  quizzes: QuizModel,
+  quizSubmissions: QuizSubmissionModel,
 };
