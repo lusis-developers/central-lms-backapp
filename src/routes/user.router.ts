0b44b610c8617b65fa537ad5b7446880592871e9
Diego Reyes <diegorele13@gmail.com>
2025-12-18 02:07:43 -0500

    feat(user): add delete user endpoint with related data cleanup

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, loginUser, registerFromPayment, getUserById, checkUserByEmail, updateUser, changePassword, getUsers, grantManualAccess, requestPasswordRecovery, resetPassword, loginWithGoogle } from "../controllers/user.controller";
+import { createUser, loginUser, registerFromPayment, getUserById, checkUserByEmail, updateUser, changePassword, getUsers, grantManualAccess, requestPasswordRecovery, resetPassword, loginWithGoogle, deleteUser } from "../controllers/user.controller";
 import { verifyFirebaseToken } from "../middlewares/firebaseAuth.middleware";
 
 const userRouter = Router();
@@ -16,5 +16,7 @@ userRouter.get("/exists", checkUserByEmail);
 userRouter.get("/:userId", getUserById);
 userRouter.patch("/:userId", updateUser);
 userRouter.patch("/:userId/password", changePassword);
+// Route to delete a single user and all related data (transactions, submissions, comments, certificates)
+userRouter.delete("/:userId", deleteUser);
 
 export default userRouter;
